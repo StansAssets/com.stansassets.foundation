6a4d25b2cd51797e7a207051bb25ef681e2ecc67
Alexey Yaremenko <yaremenko@outlook.com>
2020-03-20 16:50:18 +0200

    compact way to set root VisualElement

--- Editor/Plugins/SettingsWindow/BaseTab.cs
+++ Editor/Plugins/SettingsWindow/BaseTab.cs
@@ -5,16 +5,11 @@ namespace StansAssets.Foundation.Editor
 {
     public abstract class BaseTab : VisualElement
     {
-        protected readonly VisualElement m_Root;
-
         protected BaseTab(string uxmlPath)
         {
             // Import UXML
             var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(uxmlPath);
-            var template = visualTree.CloneTree();
-            m_Root = template[0];
-
-            Add(m_Root);
+            visualTree.CloneTree(this);
         }
     }
 }
