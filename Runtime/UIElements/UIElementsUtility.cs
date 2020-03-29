b8382732e93f73e840e318c095e926f126049c8e
Stanislav Osipov <lacost.st@gmail.com>
2020-03-30 01:47:45 +0300

    Reflection Utility update (#7)
    Some utilities moved to the Runtime assembly & UIElementsUtility class added

---
+++ Runtime/UIElements/UIElementsUtility.cs
@@ -0,0 +1,46 @@
+using System.Reflection;
+using StansAssets.Foundation.Editor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace StansAssets.Foundation.UIElements
+{
+    /// <summary>
+    /// Public representation of unity UIElementsUtility class.
+    /// </summary>
+    public static class UIElementsUtility
+    {
+        /// <summary>
+        /// Create <see cref="EventBase"/> from the correspondent <see cref="Event"/> instance.
+        /// </summary>
+        /// <param name="systemEvent">System event instance.</param>
+        /// <returns>New  <see cref="EventBase"/> instance.</returns>
+        public static EventBase CreateEvent(Event systemEvent)
+        {
+            return CreateEvent(systemEvent, systemEvent.rawType);
+        }
+
+        /// <summary>
+        /// Create <see cref="EventBase"/> from the correspondent <see cref="Event"/> instance.
+        /// </summary>
+        /// <param name="systemEvent">System event instance.</param>
+        /// <param name="eventType">System event type.</param>
+        /// <returns>New  <see cref="EventBase"/> instance.</returns>
+        public static EventBase CreateEvent(Event systemEvent, EventType eventType)
+        {
+            var uiElementsUtilityType = ReflectionUtility.FindType("UnityEngine.UIElements.UIElementsUtility");
+
+            MethodInfo createEventMethodInfo = null;
+            foreach (var method in uiElementsUtilityType.GetMethods( BindingFlags.NonPublic | BindingFlags.Static))
+            {
+                if (method.Name.Equals("CreateEvent") && method.GetParameters().Length == 2)
+                {
+                    createEventMethodInfo = method;
+                    break;
+                }
+            }
+
+            return (EventBase) createEventMethodInfo?.Invoke(null, new object[]{systemEvent, eventType});
+        }
+    }
+}
---
+++ Runtime/Utilities/ReflectionUtility.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+
+namespace StansAssets.Foundation.Editor
+{
+    /// <summary>
+    /// Reflection Utility Methods.
+    /// </summary>
+    public static class ReflectionUtility
+    {
+        /// <summary>
+        /// Methods will iterate all the project Assemblies.
+        /// If typeFullName will match new object instance of that type will be created
+        /// and returned as the result.
+        /// </summary>
+        /// <param name="typeFullName">Full type name.</param>
+        /// <returns>New type instance.</returns>
+        public static object CreateInstance(string typeFullName)
+        {
+            var type = FindType(typeFullName);
+            return type != null ? Activator.CreateInstance(type) : null;
+        }
+
+        /// <summary>
+        /// Methods will iterate all the project to find a type that matches sissified full type name.
+        /// </summary>
+        /// <param name="typeFullName">Full type name.</param>
+        /// <returns>Type object.</returns>
+        public static Type FindType(string typeFullName)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            return assemblies
+                .SelectMany(assembly => assembly.GetTypes())
+                .FirstOrDefault(type => type.FullName == null || type.FullName.Equals(typeFullName));
+        }
+    }
+}
