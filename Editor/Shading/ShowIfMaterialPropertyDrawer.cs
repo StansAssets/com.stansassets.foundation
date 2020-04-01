67b1fc1209222f2e7b08694bd33cd0c272f8cdd7
Stanislav Osipov <lacost.st@gmail.com>
2020-04-01 20:19:26 +0300

    ShowIfDrawer : MaterialPropertyDrawer made public

--- Editor/Shading/ShowIfMaterialPropertyDrawer.cs
+++ Editor/Shading/ShowIfMaterialPropertyDrawer.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 using UnityEditor;
 
 namespace StansAssets.Foundation.Editor.Shading
-{ 
+{
     /// <summary>
     /// Material property attribute that allows you to display the material property
     /// only if the specified material keywords are enabled.
-    /// 
+    ///
     /// Use with **[ShowIf(KEYWORD_NAME, ...)]** before a shader property.
     /// Most often used in combination with the **[Toggle(KEYWORD_NAME)]** attribute.
     /// </summary>
@@ -17,11 +17,11 @@ namespace StansAssets.Foundation.Editor.Shading
 	///     [ShowIf(_BASE_COLOR_ON)] _BaseColor("", Color) = (1,1,1,1)
     /// </code>
     /// </example>
-    class ShowIfDrawer : MaterialPropertyDrawer
+    public class ShowIfDrawer : MaterialPropertyDrawer
     {
         string[] m_RequiredKeywords;
         bool m_IsElementHidden;
- 
+
         /// <summary>
         /// Creates ShowIfDrawer with one required keyword.
         /// </summary>
@@ -30,7 +30,7 @@ namespace StansAssets.Foundation.Editor.Shading
         {
             m_RequiredKeywords = new string[] {keyword0};
         }
- 
+
         /// <summary>
         /// Creates ShowIfDrawer with two required keywords.
         /// </summary>
@@ -40,7 +40,7 @@ namespace StansAssets.Foundation.Editor.Shading
         {
             m_RequiredKeywords = new string[] {keyword0, keyword1};
         }
- 
+
         /// <summary>
         /// Creates ShowIfDrawer with three required keywords.
         /// </summary>
@@ -51,7 +51,7 @@ namespace StansAssets.Foundation.Editor.Shading
         {
             m_RequiredKeywords = new string[] {keyword0, keyword1, keyword2};
         }
- 
+
         /// <summary>
         /// Creates ShowIfDrawer with four required keywords.
         /// </summary>
@@ -63,7 +63,7 @@ namespace StansAssets.Foundation.Editor.Shading
         {
             m_RequiredKeywords = new string[] {keyword0, keyword1, keyword2, keyword3};
         }
- 
+
         public override void OnGUI(
             Rect position,
             MaterialProperty prop,
@@ -88,7 +88,7 @@ namespace StansAssets.Foundation.Editor.Shading
                     }
                 }
             }
- 
+
             if (!m_IsElementHidden)
             {
                 editor.DefaultShaderProperty(prop, label);
@@ -99,7 +99,7 @@ namespace StansAssets.Foundation.Editor.Shading
             MaterialProperty prop,
             string label,
             MaterialEditor editor)
-        { 
+        {
             return 0;
         }
     }
