0599619d431c68277b128d544908742ee58c46c9
Alexey Yaremenko <yaremenko@outlook.com>
2020-04-02 19:28:38 +0300

    Editor/Runtime Resources folders for settings

---
+++ Editor/Core/FoundationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace StansAssets.Foundation.Editor
+{
+    public static class FoundationConstants
+    {
+        public const string RuntimeResourcesFolder = "Assets/Settings/Runtime/Resources";
+        public const string EditorResourcesFolder = "Assets/Settings/Editor/Resources";
+    }
+}
