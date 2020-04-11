69497b2e33af1e056ed53ad15c0ec0cb5f79bd17
PavelKlym <pavelklymentenko@gmail.com>
2020-04-11 19:39:15 +0300

    documentation fixes and moved PseudoStates out of VisualElementExtension class

---
+++ Runtime/UIElements/Enums/PseudoStates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StansAssets.Foundation.UIElements {
+    /// <summary>
+    /// Public representation of unity PseudoStates internal enum.
+    /// </summary>
+    [Flags]
+    public enum PseudoStates
+    {
+        Active    = 1 << 0,     // control is currently pressed in the case of a button
+        Hover     = 1 << 1,     // mouse is over control, set and removed from dispatcher automatically
+        Checked   = 1 << 3,     // usually associated with toggles of some kind to change visible style
+        Disabled  = 1 << 5,     // control will not respond to user input
+        Focus     = 1 << 6,     // control has the keyboard focus. This is activated deactivated by the dispatcher automatically
+        Root      = 1 << 7,     // set on the root visual element
+    }
+}
--- Runtime/UIElements/VisualElementExtension.cs
+++ Runtime/UIElements/VisualElementExtension.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using UnityEngine.UIElements;
 
 namespace StansAssets.Foundation.UIElements
 {
     /// <summary>
-    /// Public representation of internal VisualElement API.
+    /// VisualElement Utility Methods.
     /// </summary>
     public static class VisualElementExtension
     {
-        [Flags]
-        public enum PseudoStates
-        {
-            Active    = 1 << 0,     // control is currently pressed in the case of a button
-            Hover     = 1 << 1,     // mouse is over control, set and removed from dispatcher automatically
-            Checked   = 1 << 3,     // usually associated with toggles of some kind to change visible style
-            Disabled  = 1 << 5,     // control will not respond to user input
-            Focus     = 1 << 6,     // control has the keyboard focus. This is activated deactivated by the dispatcher automatically
-            Root      = 1 << 7,     // set on the root visual element
-        }
-
         /// <summary>
         /// Set PseudoState into visualElement to mimic input behaviour.
         /// </summary>
