c321dd43f2447ef3ebddfdae2daaab1d47250278
Alexey Yaremenko <yaremenko@outlook.com>
2020-04-12 01:44:31 +0300

    Loading Spinner Visual Element for foundation (#16)
    * Loading Spinner Visual Element for foundation

---
+++ Runtime/UIElements/Controls/LoadingSpinner.cs
@@ -0,0 +1,91 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace StansAssets.Foundation.UIElements
+{
+    /// <summary>
+    /// The LoadingSpinner control. let's you place buttons group with the labels or images
+    /// </summary>
+    public sealed class LoadingSpinner : VisualElement
+    {
+        [UsedImplicitly]
+        internal new class UxmlFactory : UxmlFactory<LoadingSpinner, UxmlTraits> { }
+
+        internal new class UxmlTraits : BindableElement.UxmlTraits { }
+
+        bool m_IsActive;
+        int m_RotationAngle;
+
+        readonly IVisualElementScheduledItem m_ScheduledUpdate;
+
+        const long k_RotationUpdateInterval = 1L;
+        const int k_RotationAngleDelta = 10;
+
+        /// <summary>
+        /// Loading Spinner control Uss class name
+        /// </summary>
+        public const string UssClassName = "stansassets-loading-spinner";
+
+        /// <summary>
+        /// Creates LoadingSpinner control
+        /// </summary>
+        public LoadingSpinner()
+        {
+            styleSheets.Add( Resources.Load<StyleSheet>("LoadingSpinner"));
+            AddToClassList(UssClassName);
+
+            m_IsActive = false;
+
+            // add child elements to set up centered spinner rotation
+            var innerElement = new VisualElement();
+            innerElement.AddToClassList("image");
+            Add(innerElement);
+
+            m_ScheduledUpdate = schedule.Execute(UpdateProgress).Every(k_RotationUpdateInterval);
+            m_ScheduledUpdate.Pause();
+
+            RegisterCallback<AttachToPanelEvent>(OnAttachToPanelEventHandler, TrickleDown.TrickleDown);
+            RegisterCallback<DetachFromPanelEvent>(OnDetachFromPanelEventHandler, TrickleDown.TrickleDown);
+        }
+
+        void OnAttachToPanelEventHandler(AttachToPanelEvent e)
+        {
+            Activate();
+        }
+
+        void OnDetachFromPanelEventHandler(DetachFromPanelEvent e)
+        {
+            Deactivate();
+        }
+
+        void UpdateProgress()
+        {
+            transform.rotation = Quaternion.Euler(0, 0, m_RotationAngle);
+            m_RotationAngle += k_RotationAngleDelta;
+            if (m_RotationAngle > 360)
+                m_RotationAngle -= 360;
+        }
+
+        void Activate()
+        {
+            if (m_IsActive)
+                return;
+
+            m_RotationAngle = 0;
+            m_ScheduledUpdate.Resume();
+
+            m_IsActive = true;
+        }
+
+        void Deactivate()
+        {
+            if (!m_IsActive)
+                return;
+
+            m_ScheduledUpdate.Pause();
+
+            m_IsActive = false;
+        }
+    }
+}
