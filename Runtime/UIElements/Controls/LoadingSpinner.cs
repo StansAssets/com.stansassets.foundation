75374afd2998a930b896602cfef21847beeb0af1
Alexey Yaremenko <yaremenko@outlook.com>
2020-04-11 10:51:47 +0300

    cleanup & comments for public endpoints

--- Runtime/UIElements/Controls/LoadingSpinner.cs
+++ Runtime/UIElements/Controls/LoadingSpinner.cs
@@ -1,74 +1,90 @@
 ﻿using JetBrains.Annotations;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace StansAssets.Foundation.UIElements
 {
+    /// <summary>
+    /// The LoadingSpinner control. let's you place buttons group with the labels or images
+    /// </summary>
     public sealed class LoadingSpinner : VisualElement
     {
-        /// <exclude/>
         [UsedImplicitly]
-        public new class UxmlFactory : UxmlFactory<LoadingSpinner, UxmlTraits> { }
+        internal new class UxmlFactory : UxmlFactory<LoadingSpinner, UxmlTraits> { }
+
+        internal new class UxmlTraits : BindableElement.UxmlTraits { }
 
         /// <summary>
-        /// <exclude/>
+        /// Returns true if control is visible and updating it's progress
         /// </summary>
-        public new class UxmlTraits : BindableElement.UxmlTraits { }
-
         public bool Started { get; private set; }
 
-        int m_Rotation;
+        readonly IVisualElementScheduledItem m_ScheduledUpdate;
+        int m_RotationAngle;
+
+        const long k_RotationUpdateInterval = 1L;
+        const int k_RotationAngleDelta = 10;
 
         /// <summary>
         /// Loading Spinner control Uss class name
         /// </summary>
         public const string UssClassName = "stansassets-loading-spinner";
 
+        /// <summary>
+        /// Creates LoadingSpinner control
+        /// </summary>
         public LoadingSpinner()
         {
             styleSheets.Add( Resources.Load<StyleSheet>("LoadingSpinner"));
             AddToClassList(UssClassName);
 
             Started = false;
-            UIUtils.SetElementDisplay(this, false);
+            style.display = DisplayStyle.None;
 
             // add child elements to set up centered spinner rotation
             var innerElement = new VisualElement();
             innerElement.AddToClassList("image");
             Add(innerElement);
+
+            m_ScheduledUpdate = schedule.Execute(UpdateProgress).Every(k_RotationUpdateInterval);
+            m_ScheduledUpdate.Pause();
         }
 
         void UpdateProgress()
         {
-            transform.rotation = Quaternion.Euler(0, 0, m_Rotation);
-            m_Rotation += 3;
-            if (m_Rotation > 360)
-                m_Rotation -= 360;
+            transform.rotation = Quaternion.Euler(0, 0, m_RotationAngle);
+            m_RotationAngle += k_RotationAngleDelta;
+            if (m_RotationAngle > 360)
+                m_RotationAngle -= 360;
         }
 
+        /// <summary>
+        /// Makes control visible and starts updating it's progress
+        /// </summary>
         public void Start()
         {
             if (Started)
                 return;
 
-            m_Rotation = 0;
-
-            EditorApplication.update += UpdateProgress;
+            m_RotationAngle = 0;
+            m_ScheduledUpdate.Resume();
 
             Started = true;
-            UIUtils.SetElementDisplay(this, true);
+            style.display = DisplayStyle.Flex;
         }
 
+        /// <summary>
+        /// Hides control and stops updating it's progress
+        /// </summary>
         public void Stop()
         {
             if (!Started)
                 return;
 
-            EditorApplication.update -= UpdateProgress;
+            m_ScheduledUpdate.Pause();
 
             Started = false;
-            UIUtils.SetElementDisplay(this, false);
+            style.display = DisplayStyle.None;
         }
     }
 }
