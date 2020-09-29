16526d9755650f889f98c1a1e87861d9e812fdc8
Stanislav Osipov <lacost.st@gmail.com>
2020-09-29 15:08:16 +0300

    chore: pr feedback fixes

--- Runtime/Extensions/UnityEngine/SceneExtensions.cs
+++ Runtime/Extensions/UnityEngine/SceneExtensions.cs
@@ -5,7 +5,7 @@ namespace StansAssets.Foundation.Extensions
     /// <summary>
     /// Unity `Scene` extension methods.
     /// </summary>
-    public static class SceneExtensions 
+    public static class SceneExtensions
     {
         /// <summary>
         /// Returns the component of Type `type` in the located on scene root GameObject or any of its children using depth first search.
@@ -14,28 +14,34 @@ namespace StansAssets.Foundation.Extensions
         /// <param name="scene">Scene to operate with.</param>
         /// <typeparam name="T">Type of the component.</typeparam>
         /// <returns>A component of the matching type, if found.</returns>
-        public static T GetComponent<T>(this Scene scene) {
-            foreach (var gameObject in scene.GetRootGameObjects()) {
+        public static T GetComponentInChildren<T>(this Scene scene) where T : class
+        {
+            foreach (var gameObject in scene.GetRootGameObjects())
+            {
                 var component = gameObject.GetComponentInChildren<T>();
-                if (component != null) {
+                if (component != null)
+                {
                     return component;
                 }
             }
 
             return default;
         }
-        
+
         /// <summary>
-        /// Returns the component of Type `type` in the located on scene root GameObject.
+        /// Returns the component of Type `type` in on of the located on scene root GameObject.
         /// A component is returned only if it is found on an active GameObject.
         /// </summary>
         /// <param name="scene">Scene to operate with.</param>
         /// <typeparam name="T">Type of the component.</typeparam>
         /// <returns>A component of the matching type, if found.</returns>
-        public static T GetComponentFromRoot<T>(this Scene scene) {
-            foreach (var gameObject in scene.GetRootGameObjects()) {
+        public static T GetComponent<T>(this Scene scene) where T : class
+        {
+            foreach (var gameObject in scene.GetRootGameObjects())
+            {
                 var component = gameObject.GetComponent<T>();
-                if (component != null) {
+                if (component != null)
+                {
                     return component;
                 }
             }
