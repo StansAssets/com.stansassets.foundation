603c26d994fced1d15b35e484f2ad0a7bcdb88a2
Yurii Tor <Mortaly@bofh.od.ua>
2021-07-19 13:22:51 +0300

    fix: Blitting texture functionality is added for targets which don't support Native pointers

--- Runtime/Extensions/UnityEngine/TextureExtensions.cs
+++ Runtime/Extensions/UnityEngine/TextureExtensions.cs
@@ -16,16 +16,35 @@ namespace StansAssets.Foundation.Extensions
        
         public static Texture2D ToTexture2D(this Texture texture)
         {
-            if (texture is Texture2D) {
+            if (texture is Texture2D)
+            {
                 return texture as Texture2D;
             }
+
+            var nativePtr = texture.GetNativeTexturePtr();
+            if (nativePtr == null) // if a target platform doesn't support Native Ptr we go blitting the texture
+            {
+                Texture2D texture2D = new Texture2D(texture.width, texture.height, TextureFormat.RGBA32, false);
+                RenderTexture currentRT = RenderTexture.active;
+                RenderTexture renderTexture = RenderTexture.GetTemporary(texture.width, texture.height, 32);
+                Graphics.Blit(texture, renderTexture);
+
+                RenderTexture.active = renderTexture;
+                texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+                texture2D.Apply();
+
+                RenderTexture.active = currentRT;
+                RenderTexture.ReleaseTemporary(renderTexture);
+                return texture2D;
+            }
+            
+            // otherwise GetNativeTexturePtr is used
             return Texture2D.CreateExternalTexture(
                 texture.width,
                 texture.height,
                 TextureFormat.RGB24,
                 false, false,
-                texture.GetNativeTexturePtr());
+                nativePtr);
         }
-
     }
 }
