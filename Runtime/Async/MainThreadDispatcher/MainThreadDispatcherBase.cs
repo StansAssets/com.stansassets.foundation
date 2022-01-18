964c679628b10c1aa77454687a8812572f404fe9
Shynkiewicz <andrus.shinkiewicz@gmail.com>
2022-01-18 09:24:13 +0100

    MainThreadDispatcher #78 while instead of if + speed meter

--- Runtime/Async/MainThreadDispatcher/MainThreadDispatcherBase.cs
+++ Runtime/Async/MainThreadDispatcher/MainThreadDispatcherBase.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Concurrent;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
 
 namespace StansAssets.Foundation.Async
 {
     abstract class MainThreadDispatcherBase : IMainThreadDispatcher
     {
         static readonly ConcurrentQueue<Action> s_ExecutionQueue = new ConcurrentQueue<Action>();
+        private Stopwatch sw = new Stopwatch();
+        private float maxMillisecondsExecution = 100000;
+
 
         public abstract void Init();
 
@@ -13,8 +18,13 @@ namespace StansAssets.Foundation.Async
 
         protected void Update()
         {
-            if(s_ExecutionQueue.TryDequeue(out var action))
+            while (s_ExecutionQueue.TryDequeue(out var action) && sw.ElapsedMilliseconds < maxMillisecondsExecution)
+            {
+                sw.Start();
                 action.Invoke();
+                sw.Restart();
+                Debug.Log(sw.ElapsedMilliseconds);
+            }
         }
     }
 }
