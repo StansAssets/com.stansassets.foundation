cc9601f4faa2456104f263f32a4197055e291212
Shynkiewicz <andrus.shinkiewicz@gmail.com>
2022-01-24 11:43:30 +0100

    feat: coroutine can work in editor #79

---
+++ Runtime/Async/Coroutine/EditorCoroutineUtility.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace StansAssets.Foundation.Async
+{
+    [InitializeOnLoad]
+    public class EditorCoroutineUtility
+    {
+        private static List<IEnumerator> EditorCoroutine = new List<IEnumerator>();
+
+        public static IEnumerator StartEditorCoroutine(IEnumerator newCor)
+        {
+            EditorCoroutine.Add(newCor);
+            return newCor;
+        }
+
+        static EditorCoroutineUtility()
+        {
+            EditorApplication.update += ExecuteCoroutine;
+        }
+
+        static int currentExecute = 0;
+
+        private static void ExecuteCoroutine()
+        {
+            if (EditorCoroutine.Count <= 0)
+            {
+                return;
+            }
+
+            currentExecute = (currentExecute + 1) % EditorCoroutine.Count;
+
+            bool finish = !EditorCoroutine[currentExecute].MoveNext();
+
+            if (finish)
+            {
+                EditorCoroutine.RemoveAt(currentExecute);
+            }
+        }
+    }
+}
