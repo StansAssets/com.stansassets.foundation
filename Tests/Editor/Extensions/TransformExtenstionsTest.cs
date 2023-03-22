2a7de0c4f8438cfa31c0825673f9a4565f04572a
Roman Efrimov <roman@stansassets.com>
2023-03-22 17:59:04 +0200

    feat: transform clear test

---
+++ Tests/Editor/Extensions/TransformExtenstionsTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using StansAssets.Foundation.Extensions;
+using UnityEngine;
+
+namespace Tests.Editor.Extensions
+{
+    public class TransformExtenstionsTest
+    {
+        [Test]
+        [TestCase(0, false, ExpectedResult = 0)]
+        [TestCase(10, false, ExpectedResult = 0)]
+        [TestCase(10, true, ExpectedResult = 10)]
+        public int ClearTest(int amount, bool activeOnly)
+        {
+            //Arrange
+            Transform parentTransform = new GameObject().transform;
+
+            for (int i = 0; i < amount; i++)
+            {
+                Transform childTransform = new GameObject().transform;
+                childTransform.parent = parentTransform;
+                
+                if (activeOnly)
+                    childTransform.gameObject.SetActive(false);
+            }
+            
+            //Act
+            parentTransform.Clear(activeOnly);
+            
+            //Return
+            return parentTransform.childCount;
+        }
+    }
+}
