07c40c8d36113c837b12efd35633737e43170432
Roman Efrimov <roman@stansassets.com>
2023-03-24 16:43:24 +0200

    feat: base inspector button implementation

---
+++ Editor/ObjectCustomEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using StansAssets.Foundation.UIElements;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace StansAssets.Foundation.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(Object), true)]
+    public class ObjectCustomEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            DrawInspectorButtons();
+        }
+
+        private void DrawInspectorButtons()
+        {
+            var type = target.GetType();
+
+            foreach (var method in type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static))
+            {
+                var buttonAttribute = method.GetCustomAttribute<ButtonAttribute>();
+                var parametersCount = method.GetParameters().Length;
+
+                if (buttonAttribute != null && parametersCount == 0)
+                {
+                    if (GUILayout.Button(buttonAttribute.Name))
+                    {
+                        method.Invoke(target, null);
+                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/UIToolkit/Button/ButtonAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace StansAssets.Foundation.UIElements
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ButtonAttribute : PropertyAttribute
+    {
+        public readonly string Name;
+
+        public ButtonAttribute(string name = default)
+        {
+            Name = name;
+        }
+    }
+}
